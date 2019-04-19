1c7add93f4c398ff9564407e71b95fce6c46944e
bangkai <ray5527880@gmail.com>
2019-04-19 17:47:19 +0800

    4/19

---
+++ bigtal/test/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "獲利：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "支出：";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(87, 83);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(132, 23);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "輸入：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "推薦下注：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "獎號";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(373, 454);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ bigtal/test/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> number;
+        public decimal decProfit;
+        public decimal decOutlay;
+        public decimal decOdds = 6.61m;
+        public decimal decBase = 10;
+        public List<int> pu;
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            number = new List<string>();
+            pu = new List<int>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNumber.Text.Length == 5)
+            {
+                if (number.Count >= 5)
+                {
+                    count();
+                }
+                else
+                {
+                    number.Add(txtNumber.Text);
+                    txtNumber.Text = "";
+                }
+                UpdataView();
+            }
+            else
+            {
+                MessageBox.Show("輸入錯誤");
+            }
+        }
+        public void UpdataView()
+        {
+            if (number.Count >= 5)
+            {
+
+            }
+            string strOut = "獎號："+"\n";
+
+            foreach (var item in number)
+            {
+                strOut += item.ToString() + "\n";
+            }
+            label5.Text = strOut;
+        }
+        public void count()
+        {
+            if (pu.Count > 0)
+            {
+                int countNumber = 0;
+                foreach (var item in pu)
+                {
+                    if (txtNumber.Text.IndexOf(item.ToString()) > -1)
+                    {
+                        countNumber++;
+                    }
+                }
+                decOutlay += decBase * 3;
+                if (countNumber == 2)
+                    decProfit += decBase * decOdds;
+                if (countNumber == 3)
+                    decProfit += decBase * decOdds * 3;
+                label1.Text = "支出：" + decOutlay;
+                label2.Text = "獲利：" + decProfit;
+            }
+            
+            if (number.Count >= 5)
+            {
+                int[] m = new int[10];
+                foreach (var item in number)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if ((item.ToString()).Substring(j, 1) == i.ToString())
+                            {
+                                m[i]++;
+                            }
+                        }
+                    }
+                }
+                pu.Clear();
+                int no1 = -1;
+                int no2 = -1;
+                int no3 = -1;
+                for (int j = 0; j < 3; j++)
+                {
+                    int max = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (no1 != i && no2 != i)
+                        {
+                            if (m[i] > max)
+                            {
+                                if (j == 0)
+                                    no1 = i;
+                                if (j == 1)
+                                    no2 = i;
+                                if (j == 2)
+                                    no3 = i;
+                                max = m[i];
+                            }
+                        }
+                    }
+                }
+                pu.Add(no1);
+                pu.Add(no2);
+                pu.Add(no3);
+                label4.Text = "推薦下注：" + pu[0].ToString() + pu[1].ToString() + pu[2].ToString();
+                number.RemoveAt(0);
+                number.Add(txtNumber.Text);
+                txtNumber.Text = "";
+            }
+        }
+    }
+}
