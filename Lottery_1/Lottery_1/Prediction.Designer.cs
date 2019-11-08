80a583720a32d27eb5be570edc6e1fac3a076bb0
bangkai <ray5527880@gmail.com>
2019-11-08 16:53:19 +0800

    1108

--- Lottery_1/Lottery_1/Prediction.Designer.cs
+++ Lottery_1/Lottery_1/Prediction.Designer.cs
@@ -28,6 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,6 +42,11 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -97,7 +108,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(38, 224);
+            this.label6.Location = new System.Drawing.Point(35, 224);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(46, 16);
             this.label6.TabIndex = 6;
@@ -106,16 +117,80 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 260);
+            this.label7.Location = new System.Drawing.Point(346, 108);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(46, 16);
             this.label7.TabIndex = 7;
             this.label7.Text = "label7";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(346, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(346, 184);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(346, 224);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 16);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "label10";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 243);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(602, 248);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
             // Prediction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -129,6 +204,7 @@
             this.Name = "Prediction";
             this.Size = new System.Drawing.Size(840, 660);
             this.Load += new System.EventHandler(this.Prediction_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,5 +220,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Lottery_1/Lottery_1/Prediction.cs
+++ Lottery_1/Lottery_1/Prediction.cs
@@ -6,7 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Windows.Forms.DataVisualization.Charting;
 using Lottery;
 
 namespace Lottery_1
@@ -40,6 +40,7 @@ namespace Lottery_1
                 label4.Text = "";
             }
             maxNo = List539.Max(ee => ee.No);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -62,34 +63,33 @@ namespace Lottery_1
             string str = string.Empty;
             int max1 = 0;
             int max2 = 0;
-            foreach (var item in endnumber.OrderByDescending(e=>e.count))
-            {
-                str += item.No + "：" + item.count+"  ";
-                if (max1 == 0)
-                    max1 = item.No;
-                else if (max2 == 0)
-                    max2 = item.No;
-            }
-            label2.Text = str;
-            
+            //foreach (var item in endnumber.OrderByDescending(e=>e.count))
+            //{
+            //    //str += item.No + "：" + item.count+"  ";
+            //    if (max1 == 0)
+            //        max1 = item.No;
+            //    else if (max2 == 0)
+            //        max2 = item.No;
+            //}
+           
             //List<L539> list539 = new List<L539>();
             int count = 0;
-            label3.Text ="";
+            label3.Text =" ";
             foreach (var item in (LMath.GetCountList(list100)).OrderByDescending(e=>e.count))
             {
                 label3.Text += " " + item.No + " ";
                 count++;
-                if (count == 13)
+                if (count == 6)
                     break;
             }
-            label5.Text = "";
+            label5.Text = " ";
             count = 0;
             foreach (var item in (LMath.GetCountList(list10)).Where(e=>e.count>1).OrderByDescending(e => e.count))
             {
                 count++;
                 label5.Text += " " + item.No + " ";
             }
-            label6.Text = "";
+            label6.Text = " ";
             count = 0;
             foreach (var item in LMath.GetNextNumber(list100,list100.ElementAt(0)).OrderByDescending(e=>e.count))
             {
@@ -113,6 +113,117 @@ namespace Lottery_1
                 if (t1 && t2 && t3)
                     label7.Text += i.ToString() + " , ";
             }
+
+            if (ccc > 0)
+            {
+                string st1, st2, st3;
+                st1 = st2 = st3 = string.Empty;
+                label2.Text = "";
+                foreach (var item in List539.Where(e => e.No == maxNo - ccc + 1))
+                {
+                    label2.Text = " " + item.n_1 + " " + " " + item.n_2 + " " + " " + item.n_3 + " " + " " + item.n_4 + " " + " " + item.n_5 + " ";
+                    label8.Text = ch(label3.Text, item);
+                    label9.Text = ch(label5.Text, item);
+                    label10.Text = ch(label6.Text, item);
+                }                    
+            }
+            count=1;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart1.Series[3].Points.Clear();
+            foreach (var item in list20.OrderBy(e => e.No))
+            {
+                int sum = item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5;
+                chart1.Series[0].Points.AddXY(count, sum);
+
+                var _list10 = LMath.GetTOPList(List539, limetNo - 11 + count, 10);
+
+                chart1.Series[1].Points.AddXY(count, GetAVG(_list10));
+                chart1.Series[2].Points.AddXY(count, GetAVGUP(_list10));
+                chart1.Series[3].Points.AddXY(count, GetAVGDOWN(_list10));
+
+                count++;
+
+
+
+                //chart1.Series[0].ChartType=SeriesChartType.Line
+            }
+        }
+
+        private double GetAVG(List<Number.Number_539> m539)
+        {
+            double reDoub = 0;
+
+            int sum = 0;
+            foreach (var item in m539)
+            {
+                sum += item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5;
+            }
+            reDoub = sum / 10;
+
+            return reDoub;
+        }
+        private double GetAVGUP(List<Number.Number_539> m539)
+        {
+            double reDoub = 0;
+
+            int sum = 0;
+            foreach (var item in m539)
+            {
+                sum += item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5;
+            }
+            reDoub = sum / 10;
+
+            double dou = 0;
+            foreach (var item in m539)
+            {
+                dou += Math.Pow((item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5 - reDoub), 2);
+            }
+            dou = dou / 10;
+            dou = Math.Sqrt(dou);
+            reDoub += dou * 2;
+
+            return reDoub;
+        }
+        private double GetAVGDOWN(List<Number.Number_539> m539)
+        {
+            double reDoub = 0;
+
+            int sum = 0;
+            foreach (var item in m539)
+            {
+                sum += item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5;
+            }
+            reDoub = sum / 10;
+
+            double dou = 0;
+            foreach (var item in m539)
+            {
+                dou += Math.Pow((item.n_1 + item.n_2 + item.n_3 + item.n_4 + item.n_5 - reDoub), 2);
+            }
+            dou = dou / 10;
+            dou = Math.Sqrt(dou);
+            reDoub -= dou * 2;
+
+            return reDoub;
+        }
+        private string ch(string number, Number.Number_539 nowNumber)
+        {
+            string reStr = string.Empty;
+
+            if (number.IndexOf(" " + nowNumber.n_1 + " ") > 0)
+                reStr += nowNumber.n_1 + ",";
+            if (number.IndexOf(" " + nowNumber.n_2 + " ") > 0)
+                reStr += nowNumber.n_2 + ",";
+            if (number.IndexOf(" " + nowNumber.n_3 + " ") > 0)
+                reStr += nowNumber.n_3 + ",";
+            if (number.IndexOf(" " + nowNumber.n_4 + " ") > 0)
+                reStr += nowNumber.n_4 + ",";
+            if (number.IndexOf(" " + nowNumber.n_5 + " ") > 0)
+                reStr += nowNumber.n_5 + ",";
+
+            return reStr;
         }
     }
 }
